d6e212a0f5a256eb8c03777ccdb960e2afc56504
AlexsandarM <59208791+AlexsandarM@users.noreply.github.com>
2020-10-13 22:28:54 +0200

    prj-basic-final

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from '../product.model';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products: Product[] = [
+    new Product('A test Product', 'This is simple test', 'https://images.app.goo.gl/fjeyXDo8LGekkBeY9'),
+    new Product('A test Product', 'This is simple test', 'https://images.app.goo.gl/fjeyXDo8LGekkBeY9')
+  ];
 
   constructor() { }
 
