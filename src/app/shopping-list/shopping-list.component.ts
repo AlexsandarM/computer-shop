4979eb5243cb0b9f73982b2940a3b3fe963b4765
AlexsandarM <59208791+AlexsandarM@users.noreply.github.com>
2020-10-14 00:11:17 +0200

    prj-basic-final-last change

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Accessorie } from '../shared/accessories.model'
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  accessories: Accessorie[] = [
+    new Accessorie('Printer', 1),
+    new Accessorie('Speakers', 1)
+  ];
 
   constructor() { }
 
