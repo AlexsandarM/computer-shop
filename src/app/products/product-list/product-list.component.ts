021185aea6778c7ad42ec1fdad322a2b35f0e294
AlexsandarM <59208791+AlexsandarM@users.noreply.github.com>
2020-10-19 20:57:21 +0200

    services added

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -10,14 +11,12 @@ import { Product } from '../product.model';
 export class ProductListComponent implements OnInit {
   @Output() productWasSelected = new EventEmitter<Product>();
 
-  products: Product[] = [
-    new Product('G3 AVENGER', 'The G3 Avenger will be the centerpiece of any room with its gorgeous tempered glass window. These Desktops are prebuilt, meaning they out of our hands and into yours much faster. Allowing you to get the gear you need fast, and get in the game!', 'https://images-na.ssl-images-amazon.com/images/I/51qgpZOgN-L._AC_.jpg'),
-    new Product('G5 LITE 5', 'The G5 Lite 5 has a great translucent front panel that is perfect for RGB fans. Paired with the tempered glass side the G5 Lite 5 is a great chassis for your own personal light show.', 'https://cdn.shopify.com/s/files/1/0228/7629/1136/products/V200Ryzen_2000x.png?v=1592585583')
-  ];
+  products: Product[];
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.products = this.productService.getProduct();
   }
 
   onProductSelected(product: Product) {
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  private products: Product[] = [
+    new Product('G3 AVENGER', 'The G3 Avenger will be the centerpiece of any room with its gorgeous tempered glass window. These Desktops are prebuilt, meaning they out of our hands and into yours much faster. Allowing you to get the gear you need fast, and get in the game!', 'https://images-na.ssl-images-amazon.com/images/I/51qgpZOgN-L._AC_.jpg'),
+    new Product('G5 LITE 5', 'The G5 Lite 5 has a great translucent front panel that is perfect for RGB fans. Paired with the tempered glass side the G5 Lite 5 is a great chassis for your own personal light show.', 'https://cdn.shopify.com/s/files/1/0228/7629/1136/products/V200Ryzen_2000x.png?v=1592585583')
+  ];
+
+  getProduct() {
+    return this.products.slice();
+  }
+
+}
